b0f58c50fbb1e262abd88ad0eb78c17cbc215786
leush <leushkinml@mail.ru>
2022-12-14 12:16:32 +0300

    Initial commit

---
+++ src/main/java/com/skypro/algorithm2lion/Algorithm2LionApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.algorithm2lion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algorithm2LionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Algorithm2LionApplication.class, args);
+	}
+
+}
